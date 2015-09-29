9f0bbbe84bf18e2f147d19343652e96b16617397
Laurel Di Fonzo <lbd5068@psu.edu>
2015-09-29 18:41:05 -0400

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -3,6 +3,21 @@ window.onresize = resize;
 function resize(){
  load();
 }
+function render(){
+ var ctx = document.getElementById('canvas').getContext('2d');
+  ctx.save();
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = '#325FA2';
+  ctx.clearRect(0,0,ctx.canvas.clientWidth,ctx.canvas.clientHeight);//get actual nums
+  ctx.beginPath();
+  for(var x=0;x<10;x++){//
+   for(var y=0;y<10;y++){//
+    ctx.rect(x*11+mouse.x/5.0,y*11+mouse.y/5.0,10,10);
+   }
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
 var mouse={
  x:0,
  y:0
@@ -21,27 +36,15 @@ function getMousePos(canvas, evt) {
 }
 function clock(){
   //var now = new Date();
-  var ctx = document.getElementById('canvas').getContext('2d');
-  ctx.save();
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = '#325FA2';
-  ctx.clearRect(0,0,ctx.canvas.clientWidth,ctx.canvas.clientHeight);//get actual nums
-  ctx.beginPath();
-  for(var x=0;x<10;x++){//
-   for(var y=0;y<10;y++){//
-    ctx.rect(x*11+mouse.x/5.0,y*11+mouse.y/5.0,10,10);
-   }
-  }
-  ctx.closePath();
-  ctx.stroke();
+  render();
   canvas.addEventListener('click', function(e){
    mouse=getMousePos(canvas,e);
-   clock();
+   render();
 });
 
   ctx.restore();
 
-  window.requestAnimationFrame(clock);
+//  window.requestAnimationFrame(clock);
 }
 
-window.requestAnimationFrame(clock);
+//window.requestAnimationFrame(clock);
