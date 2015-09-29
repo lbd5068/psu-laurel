e240c9ff4b4a5c8503b6b37be808e0e5b2aeba46
Laurel Di Fonzo <lbd5068@psu.edu>
2015-09-29 15:06:34 -0400

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -17,7 +17,7 @@ function clock(){
   ctx.clearRect(0,0,150,150);//get actual nums
   for(var x=0;x<10;x++){//don't trust no auto semicoloning
    for(var y=0;y<10;y++){//
-    ctx.fillRect(x*110,y*110,100,100);
+    ctx.fillRect(x*11,y*11,10,10);
    }
   }
   canvas.addEventListener('mousemove', function(e){
