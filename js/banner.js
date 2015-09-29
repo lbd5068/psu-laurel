8c0cd66144ce3d912baa7d2edba625f24deb85c1
Laurel Di Fonzo <lbd5068@psu.edu>
2015-09-29 16:11:33 -0400

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -26,11 +26,13 @@ function clock(){
   ctx.lineWidth = 14;
   ctx.strokeStyle = '#325FA2';
   ctx.clearRect(0,0,ctx.canvas.clientWidth,ctx.canvas.clientHeight);//get actual nums
+  ctx.beginPath();
   for(var x=0;x<10;x++){//
    for(var y=0;y<10;y++){//
     ctx.fillRect(x*11+mouse.x,y*11+mouse.y,10,10);
    }
   }
+  ctx.endPath();
   canvas.addEventListener('mousemove', function(e){
    mouse=getMousePos(canvas,e);
    clock();
